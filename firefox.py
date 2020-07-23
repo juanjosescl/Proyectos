6100ee8537debe3fea0d2085f27d28795d0f1a48
Juanjo <juanolate01@gmail.com>
2020-07-23 17:31:38 -0400

    Primera prueba git

---
+++ firefox.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from time import sleep
+
+browser = webdriver.Firefox(executable_path=r"C:\Users\jolate\Desktop\Proyectos\geckodriver-v0.26.0-win64\geckodriver.exe")
+browser.get("https://www.google.com")
+
+
+
+# def prueba():w
+#     webdriver driver
+#     System.setProperty("webdriver.gecko.driver","C:\Users\jolate\Desktop\Proyectos\geckodriver-v0.26.0-win64\geckodriver.exe");
+#     driver = new FirefoxDriver()
+#     driver.get('https://youtube.com')
+
+
+
+# prueba()
+
+
+
+
+
