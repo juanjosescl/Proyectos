78f8948e212999b475a24e90b0622be445c9ca6d
Juanjo <juanolate01@gmail.com>
2020-07-23 17:54:04 -0400

    prueba

--- firefox.py
+++ firefox.py
@@ -4,7 +4,7 @@ from time import sleep
 browser = webdriver.Firefox(executable_path=r"C:\Users\jolate\Desktop\Proyectos\geckodriver-v0.26.0-win64\geckodriver.exe")
 browser.get("https://www.google.com")
 
-
+print(browser)
 
 # def prueba():w
 #     webdriver driver
